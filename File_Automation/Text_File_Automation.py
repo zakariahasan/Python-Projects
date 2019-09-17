9fd90fb2d6272ebad8191c78d7915d65cdaeea8a
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2019-09-17 22:54:16 +0300

    Create Text_File_Automation.py

---
+++ File_Automation/Text_File_Automation.py
@@ -0,0 +1,15 @@
+with open("/Volumes/Z/T1/Lab-3-2_configuration.txt","r") as f:
+
+    line = f.readline()
+    cnt = 1
+
+    while line:
+        x, y = f.readline().split("#")
+        line = f.readline()
+        cnt += 1
+
+        with open("/Volumes/Z/T1/Lab-3-2_configuration_new.txt", "a") as f2:
+            f2.write(y)
+            f2.close()
+
+    f.close()
